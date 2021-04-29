149571bbc6eaf0e59b7d1492380acfd5a562b1d3
Hardik <shahhard1005@gmail.com>
2021-04-29 18:20:48 +0530

    Start

---
+++ contracts/MediChain.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.5.0;
+
+contract MediChain{
+    
+    uint public count = 0;
+    struct Data{
+        uint id;
+        string record;
+    }
+    
+    mapping(uint => Data) public data;
+    
+    constructor() public{
+        create_record(count , "The Genisis block data");
+    }
+    
+    function create_record(uint id , string memory _record)public{
+        
+        data[count] = Data(id , _record);
+        count++;
+    }   
+    
+}
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+const MediChain = artifacts.require("./MediChain.sol");
+
+module.exports = function(deployer) {
+  deployer.deploy(MediChain);
+};
